19050bf8270435291273474ab10e19f6564ee7f2
amalshweiki <amalshweiki88@gmail.com>
2024-01-30 02:07:41 +0200

    final

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import CakeView from "../features/cake/CakeView"
+import IcecreamView from '../features/iceCream/IcecreamView'
+import UserList from '../features/user/UserList'
+function App() {
+
+
+  return (
+    <div>
+     
+      <CakeView/>
+      <IcecreamView/>
+      <UserList/>
+    </div>
+  )
+}
+
+export default App
